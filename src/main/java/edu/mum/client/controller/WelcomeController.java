277676e49ad06f900b4e80286da8d2c7fa596eab
edwingbd <edwingbd@gmail.com>
2019-06-15 12:02:11 -0500

    little changes
    runt ove the 808

---
+++ src/main/java/edu/mum/client/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package edu.mum.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/welcome")
+    private String start(){
+        return "login";
+    }
+}

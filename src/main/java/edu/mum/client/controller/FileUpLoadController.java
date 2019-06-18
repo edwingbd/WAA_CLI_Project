d14e8883fcca3f63df8019812daec8dc92553247
edwingbd <edwingbd@gmail.com>
2019-06-17 22:52:04 -0500

    some change
    fixed the problems with the file is not finish but it will be jijijij

--- src/main/java/edu/mum/client/controller/FileUpLoadController.java
+++ src/main/java/edu/mum/client/controller/FileUpLoadController.java
@@ -1,15 +1,60 @@
 package edu.mum.client.controller;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@RequestMapping("/fileUpLoad")
 @Controller
 public class FileUpLoadController {
 
-    @GetMapping("/fileUpLoad")
+    @GetMapping("/")
     private String showForm()
     {
+        System.out.println("showForm()");
         return "uploadfiles" ;
+    }
+
+    @PostMapping(value = "/uploadFile")
+    public String submit(@RequestParam("file") MultipartFile file) throws IOException {
+        System.out.println("step1");
+        byte[] bytes = file.getBytes();
+/*        Path path = Paths.get(Constants.UPLOAD + file.getOriginalFilename());
+        Files.write(path, bytes);*/
+
+        // prepare the file that receive to send to the server
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file",new ByteArrayResource(bytes));
+
+        // it must chage to the server url.
+        String serverUrl = "http://localhost:8081/api/uploadFile/";
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        //headers.set("Authorization", "Bearer " + tokenHelper.getToken());
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+
+
+
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+//
+        //      ResponseEntity<String> response = restTemplate.postForEntity(serverUrl, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(serverUrl, HttpMethod.POST, requestEntity, String.class);
+        return "redirect:/";
     }
 }
--- src/main/resources/static/js/uploadfilesMain.js
+++ src/main/resources/static/js/uploadfilesMain.js
@@ -1,24 +1,17 @@
 $(document).ready(function () {
     console.log("Run");//send
     $("#uploadForm").submit(function (e) {
-
-
         e.preventDefault();
         console.log("send file");
-
-
-
-
         $.ajax({
-            url: "http://localhost:8081/api/v1/file/upload/",
+            url: "http://localhost:8081/api/uploadFile",
             type: "POST",
             data: new FormData(this),
             contentType: false,
             cache: false,
             processData: false,
             headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Authorization':'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0aW5hQG11bS5lZHUiLCJ1c2VybmFtZSI6InRpbmFAbXVtLmVkdSIsInJvbGVzIjpbIlNBVkVfRU5UUlkiLCJTQVZFX0JMT0NLIiwiVklFV19FTlRSWV9SRVBPUlQiLCJWSUVXX0VYVFJBX0NSRURJVF9SRVBPUlQiLCJEQVRBX0lNUE9SVCJdLCJpZCI6Mn0.QHG5qpsyIema8mllRUW7bV7bkr2AlVioFJnpZS6uoy50ahz9lPdpu7C7U1A3lUaeDdrxCAZHmZ-dxw92wIUbPQ'
+                'Access-Control-Allow-Origin': '*'
             },
             success:function () {
                 alert("OK")
